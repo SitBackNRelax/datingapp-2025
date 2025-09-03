40dab08e39d2fe08dc4f68eba1544f0713f1aeb7
Stan Foo <stanfoo@nubitel.co>
2025-09-03 15:49:59 +0800

    End of section 10

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Exetnsions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,13 +10,13 @@ namespace API.Controllers
 {
     [Authorize]
     public class MembersController(IMemberRepository memberRepository) : BaseAPIController
-    {        
+    {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
             return Ok(await memberRepository.GetMembersAsync());
         }
-        
+
         [HttpGet("{id}")] // localhost:5001/api/members/bob-id
         public async Task<ActionResult<Member>> GetMembers(string id)
         {
@@ -28,6 +31,29 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotoForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+
+            if (member == null) return BadRequest("Could not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+            memberRepository.Update(member); //optional
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update member");
         }
     }
 }
--- Interfaces/IMemberRepository.cs
+++ Interfaces/IMemberRepository.cs
@@ -11,5 +11,5 @@ public interface IMemberRepository
     Task<IReadOnlyList<Member>> GetMembersAsync();
     Task<Member?> GetMemberByIdAsync(string id);
     Task<IReadOnlyList<Photo>> GetPhotoForMemberAsync(string memberId);
-
+    Task<Member?> GetMemberForUpdate(string Id);
 }
